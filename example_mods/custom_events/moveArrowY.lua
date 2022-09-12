1dc9a11121d8bdf664bbc7edae403b5095fcbd0b
funtime-foxy1 <dldilley01@outlook.com>
2022-09-11 20:04:19 -0400

    events and characters

---
+++ example_mods/custom_events/moveArrowY.lua
@@ -0,0 +1,6 @@
+function onEvent(name, value1, value2)
+	
+	
+	noteTweenY("noteScaleTweenY", value1, value2, 1, "sineInOut")
+	
+end
---
+++ example_mods/custom_events/moveArrowZ.lua
@@ -0,0 +1,6 @@
+function onEvent(name, value1, value2)
+	
+	
+	noteTweenZ("noteScaleTweenZ", value1, value2, 1, "sineInOut")
+	
+end
